286834d0b6afab9fff1ac907cc3dfe81eaf9e934
Jay G <92742742+ckbailar7@users.noreply.github.com>
2024-12-08 13:01:31 -0500

    Simplify App.jsx and Main with Proper Routing

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
+import "../blocks/App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div id="content__container"></div>;
+}
+
+export default App;
