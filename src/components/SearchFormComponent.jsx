27608cb46b44754bf3ab7da6820497c4f4d713ee
Jay G <92742742+ckbailar7@users.noreply.github.com>
2024-12-15 19:54:48 -0500

    Dec 15 updating SearchFormComponent styles

--- src/components/SearchFormComponent.jsx
+++ src/components/SearchFormComponent.jsx
@@ -3,8 +3,10 @@ import "../blocks/SearchFormComponent.css";
 const SearchFormComponent = ({}) => {
   return (
     <div className="SearchFormComponent__container">
-      <h1>What's going on in the world?</h1>
-      <h2>
+      <h1 className="SearchFormComponent__container-header_1">
+        What's going on in the world?
+      </h1>
+      <h2 className="SearchFormComponent__container-header_2">
         Find the latest news on any topic and save them to your personal account
       </h2>
       <div className="SearchFormComponent__Search-Bar__container">
