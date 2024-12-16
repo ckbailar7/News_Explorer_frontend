5941a2bb1246e1ad3ee9cbd49701d105c32601ff
Jay G <92742742+ckbailar7@users.noreply.github.com>
2024-12-16 14:14:58 -0500

    Dec 16th commit 2 About section

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -4,16 +4,18 @@ import AboutPhoto from "../assets/AboutPlaceholderPhoto.svg";
 const AboutComponent = ({}) => {
   return (
     <div className="AboutComponent__container">
-      <div className="AboutComponent__image">
-        <img src={AboutPhoto} alt="" />
+      <div className="AboutComponent__image_wrapper">
+        <img className="AboutComponent__image" src={AboutPhoto} alt="" />
       </div>
-      <h1 className="AboutComponent__Header1">About the author</h1>
-      <p className="AboutComponent__paragraph">
-        This block describes the project author. Here you should indicate your
-        name, what you do, and which development technologies you know. You can
-        also talk about your experience with TripleTen, what you learned there,
-        and how you can help potential customers.
-      </p>
+      <div className="AboutComponent__HeadersWrapper">
+        <h1 className="AboutComponent__Header1">About the author</h1>
+        <p className="AboutComponent__paragraph">
+          This block describes the project author. Here you should indicate your
+          name, what you do, and which development technologies you know. You
+          can also talk about your experience with TripleTen, what you learned
+          there, and how you can help potential customers.
+        </p>
+      </div>
     </div>
   );
 };
