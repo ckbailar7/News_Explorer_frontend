ae0230e2e2513fb733942b2a60686fc71b6f38ae
Jay G <92742742+ckbailar7@users.noreply.github.com>
2024-12-20 09:51:01 -0500

    Fixing initial new-card positioning

--- src/components/NewsCardComponent.jsx
+++ src/components/NewsCardComponent.jsx
@@ -1,7 +1,22 @@
+import "../blocks/NewsCardComponent.css";
+
 const NewsCardComponent = ({}) => {
-  <div className="NewsCard__container">
-    <div className="NewsCard__container_"></div>
-  </div>;
+  return (
+    <div className="news-card">
+      <h1>Search results</h1>
+      <div className="news-card__list">
+        <div className="news-card__item">
+          <div className="news-card__image-container">
+            <img className="news-card__image" src="" alt="" />
+          </div>
+        </div>
+        <div className="news-card__details">
+          <h2 className="news-card__title">TitleSubForVariable</h2>
+          <p className="news-card__description"></p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NewsCardComponent;
