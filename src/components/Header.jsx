83efe205c3098962e2e33d2791ce56efa7355a2b
Jay G <92742742+ckbailar7@users.noreply.github.com>
2025-01-16 21:25:08 -0500

    BEM fixes progress 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,25 +18,55 @@ const Header = ({ isLoggedIn, onCreateLoginModal, closeModal }) => {
     ? "/src/assets/NewsExplorerNewsExplorerHeaderDark.svg"
     : "/src/assets/headerLogoWhite.svg";
 
+  const headerHomeButtonChange = isProfilePage
+    ? "header__homeButton-profilePage"
+    : "header__homeButton";
+
+  const headerSavedArticlesButtonChange = isProfilePage
+    ? "header__savedArticlesButton--profilePage"
+    : "header__savedArticlesButton";
+
+  const navigation = isLoggedIn ? (
+    <nav className="header__navigation header__navigation--logged-in">
+      <div className="header__homeButton--container">
+        <NavLink to="/">
+          <button className={headerHomeButtonChange}>Home</button>
+        </NavLink>
+      </div>
+      <div className="header__savedArticlesButton--container">
+        <NavLink to="/profile">
+          <button className={headerSavedArticlesButtonChange}>
+            Saved Articles
+          </button>
+        </NavLink>
+      </div>
+      <div className="header__profileButton--container">
+        <NavLink to="/profile">
+          <button className="header__profileButton">ProfileName</button>
+        </NavLink>
+        <button className="header__logoutButton"></button>
+      </div>
+    </nav>
+  ) : (
+    <nav className="header__navigation">
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/register">Register</NavLink>
+    </nav>
+  );
+
   return (
-    <header className="header__container">
-      <div className="header__upperbar">
-        <div className="header__logo">
-          <div className="header__logo header__logo_white">
-            <NavLink to="/">
-              <img
-                src={headerLogoChange}
-                className="header__logo_white_image"
-                alt="header logo"
-              />
-            </NavLink>
-          </div>
+    <header className="header">
+      <div className="header__top-bar">
+        <div className="header__logo header__logo--white">
+          <NavLink to="/">
+            <img
+              src={headerLogoChange}
+              alt="header logo"
+              className="header__logo-image"
+            />
+          </NavLink>
         </div>
-        {isLoggedIn ? (
-          <NavigationComponentLoggedIn />
-        ) : (
-          <NavigationComponent onCreateLoginModal={onCreateLoginModal} />
-        )}
+        {navigation}
       </div>
     </header>
   );
