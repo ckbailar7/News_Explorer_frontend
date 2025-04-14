5264046cb3d066c202b3ecbfe6ab8177b61f4c88
Jay G <92742742+ckbailar7@users.noreply.github.com>
2025-04-14 17:41:23 -0400

    Preloader typography

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -5,7 +5,7 @@ const Preloader = ({}) => {
   return (
     <div className="circle-preloader">
       <div className="circle-preloader__animation"></div>
-      <p>Searching for news...</p>
+      <p className="circle-preloader__text">Searching for news...</p>
     </div>
   );
 };
