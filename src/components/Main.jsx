122421b08eed64460d7f8838b6cdf2fdf53f92da
Jared Girvan <tkdinst.429897@gmail.com>
2025-04-24 21:04:33 -0400

    Updated Figma specs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,7 @@ import AboutComponent from '../components/AboutComponent';
 // import defaultCards from "../contexts/defaultCardArrayPrototype";
 import { useEffect, useState } from 'react';
 import Preloader from './Preloader';
+import FrownyFaceImage from '../assets/not-found_v1.svg';
 
 const Main = ({
   isLoggedIn,
@@ -45,33 +46,49 @@ const Main = ({
         <Preloader />
       ) : (
         <section className="news-section">
-          <h1 className="news-section__header">Search results</h1>
-          <div className="news-section__card-wrapper">
-            {!hasSearched ? (
-              <p className="no-search-menu">Please search Something</p>
-            ) : visibleCards.length > 0 ? (
-              visibleCards.map((card) => (
-                <NewsCardComponent
-                  key={card.id}
-                  cardData={card}
-                  isLoggedIn={isLoggedIn}
-                  onBookmarkClick={onBookmarkClick}
-                  onDeleteClick={onDeleteClick}
-                  handleSaveNewsArticle={handleSaveNewsArticle}
-                  handleRemoveNewsArticle={handleRemoveNewsArticle}
-                />
-              ))
-            ) : (
-              <p className="no-results-message">No results found</p>
-            )}
-          </div>
-          {visibleCount < cards.length && (
-            <button
-              className="news-section__show-more-button"
-              onClick={() => setVisibleCount((prev) => prev + 3)}
-            >
-              Show more
-            </button>
+          {hasSearched && visibleCards.length === 0 ? (
+            <div className="nothing-found">
+              <img
+                src={FrownyFaceImage}
+                alt="Frowny Face"
+                className="nothing-found__image"
+              />
+              <h2 className="nothing-found__header">Nothing Found</h2>
+              <p className="nothing-found__text">
+                Sorry, but nothing matched your search terms.
+              </p>
+            </div>
+          ) : (
+            <>
+              <h1 className="news-section__header">Search results</h1>
+              <div className="news-section__card-wrapper">
+                {visibleCards.length > 0 ? (
+                  visibleCards.map((card) => (
+                    <NewsCardComponent
+                      key={card.id}
+                      cardData={card}
+                      isLoggedIn={isLoggedIn}
+                      onBookmarkClick={onBookmarkClick}
+                      onDeleteClick={onDeleteClick}
+                      handleSaveNewsArticle={handleSaveNewsArticle}
+                      handleRemoveNewsArticle={handleRemoveNewsArticle}
+                    />
+                  ))
+                ) : (
+                  <p className="no-results-message">No results found</p>
+                )}
+              </div>
+              {hasSearched &&
+                visibleCount < cards.length &&
+                visibleCards.length > 0 && (
+                  <button
+                    className="news-section__show-more-button"
+                    onClick={() => setVisibleCount((prev) => prev + 3)}
+                  >
+                    Show more
+                  </button>
+                )}
+            </>
           )}
         </section>
       )}
--- src/components/SearchFormComponent.jsx
+++ src/components/SearchFormComponent.jsx
@@ -1,21 +1,18 @@
-import "../blocks/SearchFormComponent.css";
+import '../blocks/SearchFormComponent.css';
 
-const SearchFormComponent = ({ handleSearch, loading, searchQuery, setSearchQuery, errorMessage, setErrorMessage }) => {
+const SearchFormComponent = ({
+  handleSearch,
+  loading,
+  searchQuery,
+  setSearchQuery,
+  errorMessage,
+  setErrorMessage,
+}) => {
   const handleSubmit = (e) => {
-    e.preventDefault();
     const trimmedQuery = e.target.elements.search.value.trim();
-    if (!trimmedQuery) {
-      setErrorMessage("Please enter a Keyword");
-      return;
-    } 
-
-    setErrorMessage("");
+    e.preventDefault();
     handleSearch(trimmedQuery);
-    
-    
   };
-
-  console.log(typeof setSearchQuery)
 
   return (
     <section className="search-form">
@@ -32,16 +29,16 @@ const SearchFormComponent = ({ handleSearch, loading, searchQuery, setSearchQuer
           name="search"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
-          
         />
-        {errorMessage ? (<p className="search-form__error">{errorMessage}</p>): ""} 
+
         <button
           type="submit"
           className="search-form__button"
           disabled={loading}
         >
-          {loading ? "Searching ..." : "Search"}
+          {loading ? 'Searching ...' : 'Search'}
         </button>
+        {/* {errorMessage && <p className="search-form__error">{errorMessage}</p>} */}
       </form>
       {/* {errorMessage && <p className="search-form__error">{errorMessage}</p>} */}
     </section>
